ca2057fdb5794b9cbb9c22d5bed6a2a0d7a74fc2
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2020-02-08 19:41:38 +0100

    test error service

--- layers/front-layer/modules/http_request/test/http.spec.js
+++ layers/front-layer/modules/http_request/test/http.spec.js
@@ -2,6 +2,7 @@ import {describe, it, afterEach, beforeEach} from 'mocha';
 import sinon from 'sinon';
 import {expect} from 'chai';
 import HttpRequest from '../http_headers';
+import logger from '../../log_error/index';
 import airlines from '../../../mocks/airlines'
 import httpResponse from '../../../mocks/http_response'
 
@@ -10,16 +11,16 @@ describe('Test Http Request module', () => {
     let request;
     let response;
 
-    beforeEach(() => {
-        response = httpResponse(airlines);
-        sinon.stub(window, 'fetch').resolves(response);
-    });
-
     afterEach(() => {
         sinon.restore();
     });
 
     describe('Should request different Content Types', () => {
+        beforeEach(() => {
+            response = httpResponse(airlines);
+            sinon.stub(window, 'fetch').resolves(response);
+        });
+
         it('Should return a Promise that resolves json', async() => {
             sinon.spy(response, 'json');
             request = new HttpRequest(SERVICE);
@@ -36,6 +37,28 @@ describe('Test Http Request module', () => {
             sinon.assert.calledOnceWithExactly(window.fetch, SERVICE, sinon.match.has("headers"));
             sinon.assert.calledOnce(response.text);
             expect(result).to.be.equal(airlines);
+        });
+    });
+
+    describe('Should reject response when fails the request', () => {
+        const ERROR_RESPONSE = {
+            ok: false,
+            url: SERVICE,
+            status: 500,
+            statusText: 'BROKEN_FLOW'
+        };
+
+        beforeEach(() => {
+            sinon.stub(window, 'fetch').resolves(ERROR_RESPONSE);
+            sinon.stub(logger, 'message');
+        });
+
+        it('should logger error service', async() => {
+            request = new HttpRequest(SERVICE);
+            await request.json().catch((error) => {
+                sinon.assert.calledOnceWithExactly(logger.message, 'REQUEST: test, STATUS: 500-BROKEN_FLOW, PAYLOAD: ');
+                expect(error).to.be.equals(ERROR_RESPONSE);
+            });
         });
     });
 });
